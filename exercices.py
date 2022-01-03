efca88c56ae989b7bc775549408774d2c24b852e
Horoa <96960205+Horoa@users.noreply.github.com>
2022-01-03 23:49:49 +0100

    First Exercice
    Exercice one

---
+++ exercices.py
@@ -0,0 +1,8 @@
+from math import pi
+
+#1. Écrire un programme qui, à partir de la saisie d'un rayon et d'une hauteur, calcule le volume d'un cône droit.
+
+def findVolume(radius,height):
+    return pi*radius*radius*height/3.0
+  
+#2. Une boucle while : entrez un prix HT (entrez o pour terminer) et affichez sa valeur TTC.
