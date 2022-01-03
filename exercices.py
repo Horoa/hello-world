2aad3384723ff7afd5b8d15cbb808653bef2b52f
Horoa <96960205+Horoa@users.noreply.github.com>
2022-01-04 00:16:24 +0100

    Exercice 3

--- exercices.py
+++ exercices.py
@@ -8,10 +8,27 @@ def findVolume(radius,height):
 #2. Une boucle while : entrez un prix HT (entrez o pour terminer) et affichez sa valeur TTC.
 
 def findPriceTTC():
-    priceWT = float(input("Price without taxe (0 tp end) ?"))
+    priceWT = float(input("Price without taxe (0 to end) ?"))
     while priceWT > 0:
         print("Price with taxes : { :.2f}\n".format(priceWT * 1.2))
-        priceWT = float(input("Price without taxe (0 tp end) ?"))
+        priceWT = float(input("Price without taxe (0 to end) ?"))
 
 #3  Une autre boucle while : calculez la somme d'une suite de nombres positifs ou nuls. Comptez combien il y avait de données et combien étaient supérieures à 100.
 #   Entrer un nombre inférieur ou égal à 0 indique la fin de la suite.
+
+def sumOfASerie():
+    numberInput = float(input("Number of the serie (0 to end) ?"))
+    totalOfNumberHigherThanOneHundred = 0
+    totalOfNumber = 0
+    sumTotal = 0
+       
+    while numberInput > 0 :
+        if numberInput >= 100 :
+            totalOfNumberHigherThanOneHundred +=1
+        totalOfNumber +=1
+        sumTotal += numberInput
+        numberInput = float(input("Number of the serie (0 to end) ?"))
+        
+    print("The sum total of the serie is " + str(sumTotal) + " there was " + str(totalOfNumber) + " numbers and " + str(totalOfNumberHigherThanOneHundred) + " numbers higher than 100")
+
+    
