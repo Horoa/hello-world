dd6b2c0f6b3e1058364f43369376958a29cb6883
Horoa <96960205+Horoa@users.noreply.github.com>
2022-01-05 23:39:50 +0100

    Adding functions for an easy test
    If you want to test my code (like, who would ever test this code ahah),now you can just add "exerciceX()" (put the number you want for X) to test it !

--- exercices.py
+++ exercices.py
@@ -5,15 +5,22 @@ from math import pi
 
 def findVolume(radius,height):
     return pi*radius*radius*height/3.0
+
+def exercice1():
+    print("radius = " + str(findVolume(float(input("radius?")),float(input("height?")))))
   
 #2. Une boucle while : entrez un prix HT (entrez o pour terminer) et affichez sa valeur TTC.
 
 def findPriceTTC():
-    priceWT = float(input("Price without taxe (0 to end) ?"))
-    while priceWT > 0:
-        print("Price with taxes : { :.2f}\n".format(priceWT * 1.2))
-        priceWT = float(input("Price without taxe (0 to end) ?"))
+    prixHT = float(input("Prix HT (0 pour terminer) ?"))
+    while prixHT > 0:
+        print("Prix TTC : " + str(prixHT * 1.2) + "€\n")
+        prixHT = float(input("Prix HT (0 pour terminer) ?"))
 
+def exercice2():
+    findPriceTTC()
+        
+        
 #3  Une autre boucle while : calculez la somme d'une suite de nombres positifs ou nuls. Comptez combien il y avait de données et combien étaient supérieures à 100.
 #   Entrer un nombre inférieur ou égal à 0 indique la fin de la suite.
 
@@ -32,6 +39,8 @@ def sumOfASerie():
         
     print("The sum total of the serie is " + str(sumTotal) + " there was " + str(totalOfNumber) + " numbers and " + str(totalOfNumberHigherThanOneHundred) + " numbers higher than 100")
 
+def exercice3():
+    sumOfASerie()
     
 #4 L'utilisateur donne un entier positif n et le programme affiche PAIR s'il est divisible par 2, IMPAIR sinon
 
@@ -41,7 +50,9 @@ def isOddOrEven():
         print( str(numberInput) + " is even")
     else :
         print( str(numberInput) + " is odd")
-    
+
+def exercice4():
+    isOddOrEven()
     
 #5 L'utilisateur donne un entier positif et le programme annonce combien de fois de suite cet entier est divisible par 2.
 
@@ -52,7 +63,9 @@ def numberOfPossibleHalves():
         numberInput/=2
         numberOfHalves+=1
     print("You can divide your number " + str(numberOfHalves) + " times\n")
-        
+
+def exercice5():
+    numberOfPossibleHalves()
     
 #6 L'utilisateur donne un entier supérieur à 1 et le programme affiche, s'il y en a, tous ses diviseurs propres sans répétition ainsi que leur nombre. S'il n'y en a pas, il indique qu'il est premier.
 
@@ -74,6 +87,9 @@ def findCommonDivisor():
         else :
             print ( str(numberInput) + " has " + str(len(listOfCommonDivisor)) + " common divisor : " + str(listOfCommonDivisor))
             
+def exercice6():
+    findCommonDivisor()
+            
 #7 Écrire un programme qui approxime par défaut la valeur de la constante mathématique e, pour n assez grand(56)
 
 def findE():
@@ -90,6 +106,8 @@ def factoriel(number) :
         return 1
     return number*factoriel(number-1)
 
+def exercice7():
+    findE()
 
 #8  Un gardien de phare va aux toilettes cinq fois par jour. Or les WC sont au rez-de-chaussée…
 #   Écrire une procédure (donc sans return) hauteurParcourue qui reçoit deux paramètres, le nombre de marches du phare et la hauteur de chaque marche (en cm), et qui affiche :
@@ -98,6 +116,8 @@ def factoriel(number) :
 def hauteurParcourue(nombreMarche, hauteurMarche):
     print("For " + str(nombreMarche) + " steps of " + str(hauteurMarche) + "cm, he walks " + str(nombreMarche*hauteurMarche*2*7*5/100) + "m per week")
 
+def exercice8():
+    hauteurParcourue(int(input("number of step?")),float(input("height of the step?")))
    
 #9  Un permis de chasse à points remplace désormais le permis de chasse traditionnel. Chaque chasseur possède au départ un capital de 100 points. S'il tue une poule, il perd 1 point, 3 points pour un chien, 5 points pour une vache et 10 points pour un ami. Le permis coûte 200 euros.
 #   Écrire une fonction amende qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due.
@@ -106,6 +126,8 @@ def hauteurParcourue(nombreMarche, hauteurMarche):
 def calculateFine(countKillChicken, countKillDog, countKillCow, countKillHunter) :
     print(str(((countKillChicken+countKillDog*3+countKillCow*5+countKillHunter*10)//100)*200)+ "€")
 
+def exercice9():
+    calculateFine(int(input("Chicken killed?")),int(input("Dog killed?")),int(input("Cow killed?")),int(input("Hunter killed?")))
     
 #10 Je suis ligoté sur les rails en gare d'Arras. Écrire un programme qui affiche un tableau me permettant de connaître l'heure à laquelle je serai déchiqueté par le train parti de la gare du Nord à 9 h (il y a 170 km entre la gare du Nord et Arras).
 #   Le tableau prédira les différentes heures possibles pour toutes les vitesses de 100 km/h à 300 km/h, par pas de 10 km/h, les résultats étant arrondis à la minute inférieure.
@@ -121,3 +143,8 @@ def tchacatchac(trainSpeed):
 def stationTab():
     for vitesse in range(100,310,10):
         tchacatchac(vitesse)
+        
+def exercice10():
+    stationTab()
+    
+
