fa55ab89cbef9521f135497f3f12023b2520990a
Horoa <96960205+Horoa@users.noreply.github.com>
2022-01-06 00:07:32 +0100

    Exercice11

--- exercices.py
+++ exercices.py
@@ -147,4 +147,29 @@ def stationTab():
 def exercice10():
     stationTab()
     
+#11 Un programme principal saisit une chaîne d'ADN valide et une séquence d'ADN valide (valide signifie qu'elles ne sont pas vides et sont formées exclusivement d'une combinaison arbitraire de "a", "t", "g" ou "c").
+#   Écrire une fonction valide qui renvoie vrai si la saisie est valide, faux sinon.
+#   Écrire une fonction saisie qui effectue une saisie valide et renvoie la valeur saisie sous forme d'une chaîne de caractères.
+#   Écrire une fonction proportion qui reçoit deux arguments, la chaîne et la séquence et qui retourne la proportion de séquence dans la chaîne (c'est-à-dire son nombre d'occurrences).
+#   Le programme principal appelle la fonction saisie pour la chaîne et pour la séquence et affiche le résultat.
+
+def valide(chaine):   
+    if chaine == "":
+        return False
+    for letter in chaine :
+        if letter != "a" and letter != "t" and letter != "g" and letter != "c" :
+            return False
+    return True
+
+def proportion(sequence, chain):
+    return 100*chain.count(sequence)/len(chain)
+    
+def exercice11():
+    chain = str(input("your chain ?"))
+    while not(valide(chain)):
+        chain = str(input("your chain isn't valide "))
+    sequence = str(input("your sequence ?"))
+    while (not(valide(sequence)) and len(sequence) != 2) :
+        sequence = str(input("your sequence isn't valide "))
+    print ("There is " + str(proportion(sequence, chain)) + "% of " + sequence + " in your chain")
 
